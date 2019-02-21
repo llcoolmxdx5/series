a42bebf3c2ec98f8eeaa4da2b1dc6269fc0bd647
llcoolmxdx5 <2938129812@qq.com>
2019-02-21 21:51:09 +0800

    2019-2-21

--- add/main.py
+++ add/main.py
@@ -32,6 +32,12 @@ class DedeAddArticle:
     
     def __del__(self):
         self.browser.close()
+    
+    def __click(self, selector):
+        self.browser.find_element_by_css_selector(selector).click()
+    
+    def __sendkeys(self, selector, sendkeys):
+        self.browser.find_element_by_css_selector(selector).send_keys(sendkeys)
 
     def login(self):
         '''
@@ -41,13 +47,10 @@ class DedeAddArticle:
         upwd_selector = '#login-box > div.login-main > form > dl > dd:nth-child(4) > input'
         submit_selctor = '#login-box > div.login-main > form > dl > dd:nth-child(6) > button'
         self.browser.get(self.url)
-        user_name = self.browser.find_element_by_css_selector(uname_selector)
-        user_name.send_keys(self.uname) #用户名
-        user_pwd = self.browser.find_element_by_css_selector(upwd_selector)
-        user_pwd.send_keys(self.upwd) #用户密码
+        self.__sendkeys(uname_selector, self.uname)
+        self.__sendkeys(upwd_selector, self.upwd)
         # time.sleep(20)
-        submit = self.browser.find_element_by_css_selector(submit_selctor)
-        submit.click() # 登录
+        self.__click(submit_selctor)
     
     def error(self):
         self.browser.switch_to_default_content()
@@ -58,30 +61,26 @@ class DedeAddArticle:
         '''
         time.sleep(10)
         maincolumn_selector = f'body > table > tbody > tr:nth-child(4) > td > table:nth-child({self.maincolumn}) > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr > td:nth-child(1) > a:nth-child(2)'
+        webcolumn_selector = '#items1_1 > ul > li:nth-child(1) > div > div.fllct > a'
+        add_selector = 'body > table > tbody > tr > td > table > tbody > tr > td > input:nth-child(1)'
         self.browser.switch_to_frame('menufra')
-        webcolumn = self.browser.find_element_by_css_selector('#items1_1 > ul > li:nth-child(1) > div > div.fllct > a')
-        webcolumn.click()
+        self.__click(webcolumn_selector)
         self.browser.switch_to.default_content()
         self.browser.switch_to_frame('main')
         if int(self.subcolumn_selector):
             self.select_subcolumn()
         else:
-            maincolumn = self.browser.find_element_by_css_selector(maincolumn_selector)
-            maincolumn.click()
-        add_botton = self.browser.find_element_by_css_selector('body > table > tbody > tr > td > table > tbody > tr > td > input:nth-child(1)')
-        add_botton.click()
+            self.__click(maincolumn_selector)
+        self.__click(add_selector)
     
     def select_subcolumn(self):
         maincolumn_expand_selector = f'body > table > tbody > tr:nth-child(4) > td > table:nth-child({self.maincolumn}) > tbody > tr:nth-child(1) > td:nth-child(1) > img'
         subcolumn_selector = f'#suns{self.maincolumn_id} > table > tbody > tr:nth-child({str(int(self.subcolumn)*2 -1)}) > td > table > tbody > tr > td:nth-child(1) > a:nth-child(3)'
         try:
-            subcolumn_botton = self.browser.find_element_by_css_selector(subcolumn_selector)
-            subcolumn_botton.click()
+            self.__click(subcolumn_selector)
         except:
-            maincolumn_expand_botton = self.browser.find_element_by_css_selector(maincolumn_expand_selector)
-            maincolumn_expand_botton.click()
-            subcolumn_botton = self.browser.find_element_by_css_selector(subcolumn_selector)
-            subcolumn_botton.click()
+            self.__click(maincolumn_expand_selector)
+            self.__click(subcolumn_selector)
 
     def add_article(self, title, keyword, summary, L, img_L, tag=''):
         '''
@@ -93,37 +92,25 @@ class DedeAddArticle:
         save_selector = 'body > form > table:nth-child(8) > tbody > tr > td:nth-child(2) > input'
         sourcecode_selector = '#cke_8' 
         body_selector = '#cke_contents_body > textarea'
-        title_input = self.browser.find_element_by_css_selector(title_selector)
-        title_input.send_keys(title)
-        tag_input = self.browser.find_element_by_css_selector(tag_selector)
-        tag_input.send_keys(tag)
-        keyword_input = self.browser.find_element_by_css_selector(keyword_selector)
-        keyword_input.send_keys(keyword)
+        summary_selector = '#description'
+        self.__sendkeys(title_selector, title)
+        self.__sendkeys(tag_selector, tag)
+        self.__sendkeys(keyword_selector, keyword)
+        self.__sendkeys(summary_selector, summary)
         # 此处还须点击源码
-        sourcecode_input = self.browser.find_element_by_css_selector(sourcecode_selector)
-        sourcecode_input.click()
-        # self.browser.switch_to_frame(self.browser.find_element_by_xpath('//iframe[contains(@allowtransparency,"true")]'))
-        # body_input = self.browser.find_element_by_class_name('cke_show_borders')
-        body_input = self.browser.find_element_by_css_selector(body_selector)
-        body_input.click()
+        self.__click(sourcecode_selector)
         for line in L:
-            body_input.send_keys(line)
-            body_input.send_keys(Keys.ENTER)
-        # self.browser.switch_to.parent_frame() # 不再需要切换到父ifame
-        save_botton = self.browser.find_element_by_css_selector(save_selector)
-        save_botton.click()
+            self.__sendkeys(body_selector, line)
+            self.__sendkeys(body_selector, Keys.ENTER)
+        self.__click(save_selector)
 
     def continue_add(self):
         continue_selector = 'body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > div > a:nth-child(1) > u'
-        continue_botton = self.browser.find_element_by_css_selector(continue_selector)
-        continue_botton.click()
+        self.__click(continue_selector)
     
-    def add_img(self):
+    def add_img(self, imgpath):
         '''
         1.点源码
-        sourcecode_selector = '#cke_8'
-        sourcecode_input = self.browser.find_element_by_css_selector(sourcecode_selector)
-        sourcecode_input.click()
         2.点击图像 main ifame #cke_27
         3.上传 main ifame #cke_Upload_143 send_keys
         切换到子ifame
@@ -132,14 +119,32 @@ class DedeAddArticle:
         上传完后切回父ifame
         6.自动跳到图像界面选择确定 ifame#main #cke_172_label
         7.点源码
-        8.点输入框
-        body_selector = '#cke_contents_body > textarea'
-        body_input = self.browser.find_element_by_css_selector(body_selector)
-        body_input.click()
         错误：关闭 ifame#main #cke_dialog_close_button_84
         '''
-        pass
-        
+        sourcecode_selector = '#cke_8'
+        img_selector = '#cke_27'
+        imgupload_selector = '#cke_Upload_143'
+        file_selector = 'body > form > input[type="file"]'
+        upload2server_selector = '#cke_140_labelifame'
+        imgconfirm_selector = '#cke_172_label'
+        # body_selector = '#cke_contents_body > textarea'
+        # imgerror_selector = '#cke_dialog_close_button_84'
+        # error_selector = '#cke_dialog_close_button_84'
+        try:
+            self.__click(sourcecode_selector) # 1
+            self.__click(img_selector) # 2
+            self.__click(imgupload_selector) # 3
+            self.browser.switch_to_frame('cke_138_fileInput') # 4
+            self.__sendkeys(file_selector, imgpath)
+            self.__click(upload2server_selector) # 5
+            self.browser.switch_to.parent_frame()
+            self.__click(imgconfirm_selector) # 6
+            self.__click(sourcecode_selector) # 7
+        except:
+            print(f'添加图片:{imgpath} 失败')
+            self.browser.switch_to.default_content()
+            self.browser.switch_to_frame('main')
+
 
 def read_file(path1): 
     path1 = path1 + '\\'
@@ -174,10 +179,10 @@ def main(url, user, password, maincolumn, subcolumn_selector, maincolumn_id, sub
     for i in os.listdir(path):
         try:
             path1 = path + i
-            print(f'读取路径{path1}')
+            print(f'读取路径:{path1}')
             title, keyword, summary, L, img_L = read_file(path1)
             dede.add_article(title, keyword, summary, L, img_L)
-            print(f'添加文章:{title}成功,准备添加下一篇')
+            print(f'添加文章:{title} 成功,准备添加下一篇')
             dede.continue_add()
         except Exception as e:
             print(e)
