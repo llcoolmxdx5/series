bfad51fb3dec3cb55a695e9132789ee99f9068cf
llcoolmxdx5 <2938129812@qq.com>
2019-03-06 10:39:17 +0800

    2019-3-6

--- crawl/腾讯新闻/txcontent.py
+++ crawl/腾讯新闻/txcontent.py
@@ -1,4 +1,15 @@
+import requests
+from pyquery import PyQuery as pq
+import csv
+import time
+
+
+def html_download(url):
+    pass
 #Cnt-Main-Article-QQ > p:nth-child(7) 16.6内容 gb2312
+
+
+
 
 
 def main(key, file_content):
